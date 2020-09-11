78b671aedb6d13959af730f7e5ae1c7b8e8b9fa7
Ayush Gupta <721ayush@gmail.com>
2020-09-11 23:30:48 +0530

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+/*
+ *   Copyright (c) 2020
+ *   All rights reserved.
+ */
+
+import React from "react";
+import SearchBar from "./SearchBar";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/SearchBar.js
@@ -0,0 +1,13 @@
+/*
+ *   Copyright (c) 2020
+ *   All rights reserved.
+ */
+import React from "react";
+
+class SearchBar extends React.Component {
+  render() {
+    return <div>Search Bar</div>;
+  }
+}
+
+export default SearchBar;
